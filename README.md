294665976386947d75302b40d59218029a891cf2
Ailsa Meechan-Maddon <ailsamm@outlook.com>
2020-03-14 09:40:27 -0700

    Create greybox wireframes

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # Travel and Culture Quiz
 
 Here you will find an interactive and responsively-designed quiz.
+
 Test your knowledge of travel and culture as you answer a series of multiple-choice questions.
---
+++ questions.js
@@ -0,0 +1,92 @@
+const QUESTIONS = [
+    {
+        question: "What is the capital city of Scotland?",
+        options: ["London", "Edinburgh", "Glasgow", "Loch Ness"],
+        correctAnswer: "edinburgh",
+        image: ""
+    },
+    {
+        question: "In which city could you find the famous landmark Christ the Redeemer?",
+        options: ["Rio de Janeiro", "Caracas", "Mexico City", "Bogota"],
+        correctAnswer: "rio de janeiro",
+        image: ""
+    },
+    {
+        question: "Patagonia is a geographical region shared by Argentina and which other south American country?",
+        options: ["Peru", "Uruguay", "Chile", "Brazil"],
+        correctAnswer: "chile",
+        image: ""
+    },
+    {
+        question: "Tenzing-Hillary airport, rated by pilots as one of the most dangerous in the world, can be found in which country?",
+        options: ["Colombia","Belize","Iceland","Nepal"],
+        correctAnswer: "nepal",
+        image: ""
+    },
+    {
+        question: "The colourful Chichicastenango market can be found in which country?",
+        options: ["Guatemala", "Morocco", "Japan", "Uzbekistan"],
+        correctAnswer: "guatemala",
+        image: ""
+    },
+    {
+        question: "In terms of the geographical size, what is the largest US state?",
+        options: ["Rhode Island","Alaska","California","Texas"],
+        correctAnswer: "alaska",
+        image: ""
+    },
+    {
+        question: "The famous blue city, Chefchaouen, can be found in which country?",
+        options: ["Greece","Croatia","Morocco","France"],
+        correctAnswer: "morocco",
+        image: ""
+    },
+    {
+        question: "How many official languages are spoken in Switzerland?",
+        options: ["1","2","3","4"],
+        correctAnswer: "4",
+        image: ""
+    },
+    {
+        question: "What is the capital of Australia?",
+        options: ["Canberra","Sydney","Melbourne","Adelaide"],
+        correctAnswer: "canberra",
+        image: ""
+    },
+    {
+        question: "What is the capital of Switzerland?",
+        options: ["Geneva","Bern","Basel","Zurich"],
+        correctAnswer: "bern",
+        image: ""
+    },
+    {
+        question: "Which French city has the nickname 'The Pink City' thanks to its blushing pink-brick buildings?",
+        options: ["Paris","Marseille","Toulouse","Bordeaux"],
+        correctAnswer: "toulouse",
+        image: ""
+    },
+    {
+        question: "The ancient city of Constantinople is now known by what name?",
+        options: ["Jordan","Moscow","Dubai","Istanbul"],
+        correctAnswer: "",
+        image: ""
+    },
+    {
+        question: "South Africa land locks which country?",
+        options: ["Lesotho","Namibia","Ethiopia","Ghana"],
+        correctAnswer: "lesotho",
+        image: ""
+    },
+    {
+        question: "New Zealand has the highest sheep to person ratio in the world, with how many sheep per one person?",
+        options: ["3","6","9","12"],
+        correctAnswer: "9",
+        image: ""
+    },
+    {
+        question: "How many writing systems does Japan have?",
+        options: ["1","2","3","4"],
+        correctAnswer: "3",
+        image: ""
+    }
+];
